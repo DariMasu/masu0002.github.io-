1897be5d9f526354def0f0bb67f61af358e61fcc
DariMasu <masu0002@hz.nl>
2025-09-25 18:16:26 +0200

    changed faq question background

--- js/general.js
+++ js/general.js
@@ -1,6 +1,5 @@
 // open aside funtion
 function openAside() {
-  console.log("openaside clicked");
   document.getElementById("aside").style.width = "10%";
   document.getElementById("aside").style.display = "inline";
   document.getElementById("aside").style.transition = "ease-in 5s";
@@ -13,8 +12,6 @@ function closeAside() {
   document.getElementById("aside").style.width = "0";
   document.getElementById("aside").style.display = "none";
   document.getElementById("aside").style.transition = "ease-out 5s";
-  document.getElementById("main").style.width = "100%";
-  document.getElementById("main").style.marginright = "2%";
 };
 
 // starting functions on load
